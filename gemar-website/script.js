4fa27b3a7bb4116a7c976761613cde7d4038836c
GEMAR <gemargenerasimajubersinar@gmail.com>
2026-05-23 20:38:38 +0700

    Add files via upload

---
+++ gemar-website/script.js
@@ -0,0 +1,26 @@
+// MOBILE MENU
+
+const toggle = document.getElementById('toggle');
+const menu = document.getElementById('menu');
+
+toggle.onclick = () => {
+  menu.classList.toggle('active');
+}
+
+// SCROLL ANIMATION
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries)=>{
+
+  entries.forEach((entry)=>{
+
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+
+  })
+
+})
+
+hiddenElements.forEach((el)=>observer.observe(el));
